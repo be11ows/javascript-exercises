33587793999931b7aee1c42218f7dd4a99f4893e
istvan kosa <istvankosa@istvans-iMac.local>
2022-08-31 18:19:34 -0700

    Add code to pass 02 tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  let output = '';
+  if(num < 0){
+    output = 'ERROR';
+  } else {
+    for (let i = 0; i < num; i++) {
+      output += string;
+    }
+  }
+  return output;
 };
+// const number = Math.floor(Math.random() * 1000)
+repeatString('', 10)
 
 // Do not edit below this line
 module.exports = repeatString;
