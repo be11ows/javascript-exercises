241b86823c261cf320d3c8e219fe3673e9c92db0
istvan kosa <istvankosa@istvans-iMac.local>
2022-08-31 23:05:47 -0700

    Add code to pass 06_leapYears tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,23 @@
-const leapYears = function() {
+const leapYears = function(year) {
+  
+  let output;
 
+  if (year % 100 === 0) {
+    if (year % 400 === 0) {
+      output = true;
+    } else {
+      output = false;
+    }
+  } else if (year % 4 === 0) {
+    output = true;
+  } else {
+    output = false;
+  }
+
+  return output;
 };
+
+leapYears(700)
 
 // Do not edit below this line
 module.exports = leapYears;
