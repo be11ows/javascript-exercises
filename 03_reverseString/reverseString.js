52c126336e24690a752786eb5655920ac489ed4b
istvan kosa <istvankosa@istvans-iMac.local>
2022-08-31 18:28:22 -0700

    Add code to pass 03 tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  return output = string.split('').reverse().join('');
 };
+
+reverseString('');
 
 // Do not edit below this line
 module.exports = reverseString;
