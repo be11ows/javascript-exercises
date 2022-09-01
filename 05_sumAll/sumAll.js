6cfe0ce97ba969b0efc3def9767d87ec7b3ea068
istvan kosa <istvankosa@istvans-iMac.local>
2022-08-31 22:45:07 -0700

    Add code to pass 05_sumAll tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
+const sumAll = function(n1, n2) {
+  
+  if(typeof n1 !== 'number' || typeof n2 !== 'number'){
+    return 'ERROR';
+  } else if (n1 < 0 || n2 < 0) {
+    return 'ERROR';
+  }
+
+  let larger = 0;
+  let smaller = 0;
+  let sum = 0;
+
+  if (n1 > n2) {
+    larger = n1;
+    smaller = n2
+  } else {
+    smaller = n1;
+    larger = n2;
+  }
+
+  for (let i = smaller; i <= larger; i++) {
+    sum += i;
+  }
+  return sum;
 
 };
-
+sumAll(10, [90, 1]);
 // Do not edit below this line
 module.exports = sumAll;
