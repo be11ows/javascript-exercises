8c698440f1767327480e22fec3d9bd415dd96d13
istvan kosa <istvankosa@istvans-iMac.local>
2022-09-01 08:10:01 -0700

    Add code to pass 07_tempConversion tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,23 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  let c = (f - 32) * (5 / 9);
+  if(c % 1 === 0){
+    return c;
+  } else {
+    return parseFloat(c.toFixed(1));
+  }
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let f = c * (9 / 5) + 32;
+  if(f % 1 === 0){
+    return f;
+  } else {
+    return parseFloat(f.toFixed(1));
+  }
 };
+
+// ftoc(32);
+ctof(-10);
 
 // Do not edit below this line
 module.exports = {
