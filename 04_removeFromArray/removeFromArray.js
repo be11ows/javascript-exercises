46b66a6a5e6daa2b213683bb56f63b750e07e5c3
istvan kosa <istvankosa@istvans-iMac.local>
2022-08-31 22:19:42 -0700

    Add code to pass 04 tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
 
+  let args = [...arguments];
+  let arr = args.shift();
+
+  for (let i = 0; i < args.length; i++) {
+    if (arr.includes(args[i])){
+      arr.splice(arr.indexOf(args[i]), 1);
+    }
+  }
+  return arr;
 };
+
+removeFromArray([1, 2, 3], "1", 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
