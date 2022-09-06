816b6a22751413b061d9a911dea145f479b5a492
istvan kosa <istvankosa@istvans-iMac.local>
2022-09-05 18:19:35 -0700

    Add code to pass 11_getTheTitles tests

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,22 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let titlesArr = [];
+    for(let i = 0; i < books.length; i++){
+        titlesArr.push(books[i].title)
+    }
+    return titlesArr;
 };
+
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+getTheTitles(books);
 
 // Do not edit below this line
 module.exports = getTheTitles;
