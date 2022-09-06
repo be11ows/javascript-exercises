c1c3d89f1372b04bf816f339cf81303205da1158
istvan kosa <istvankosa@istvans-iMac.local>
2022-09-05 17:28:14 -0700

    Add code to pass 08_calculator tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,51 @@
-const add = function() {
-	
+const add = function(n1, n2) {
+  return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function(n1, n2) {
+	return n1 - n2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let output = 0;
+	for(let i = 0; i < arr.length; i++) {
+    output += arr[i];
+  }
+  return output;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let output = 1;
+  if(!arr){
+    return 1;
+  }
+	for(let i = 0; i < arr.length; i++) {
+    output *= arr[i];
+  }
+  return output;
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+    let output = 1;
+    for(let i = 0; i < pow; i++) {
+      output *= num;
+    }
+    return output;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+    let output = 1;
+    if(num === 0) {
+      return 1;
+    }
+    for(let i = num; i > 0; i--) {
+      output *= i;
+    }
+    return output;
 };
+
+factorial(10);
+
 
 // Do not edit below this line
 module.exports = {
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(n) { 
+    let n1 = 0, n2 = 1, next;
+    let output;
 
+        for(let i = 0; i < n; i++) {
+        if(n <= 1) {
+            return 1;
+        } else {
+            output = n2;
+            next = n1 + n2;
+            n1 = n2;
+            n2 = next;
+        }
+    }
+    return output;
 };
+
+
+
+fibonacci(4);
 
 // Do not edit below this line
 module.exports = fibonacci;
